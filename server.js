a8d40b919ebb555b66ccbb7c13f61178c258aeaf
Bubu <80535543+bubu-revelat@users.noreply.github.com>
2024-05-11 23:26:54 -0400

    update in db connected

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const db = knex({
     connection: {
         host: process.env.DB_URL,
         port: 5432,
-        ssl:true
+        ssl:true,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     },
 });
 
